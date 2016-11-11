4c99865d57689e063e05ff272bfdb59d66aea188
S.Skaugvoll <skaugvoll@gmail.com>
2016-11-11 23:28:43 +0100

    added comments

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var config = require("./_config/main.js");
-var path = require('path');
-var router = require('./router.js');
+var express = require('express'); // Import express server framework
+var config = require("./_config/main.js"); // Import configurations
+var path = require('path'); // Import path-module
+var router = require('./router.js'); // Import main router with all routes
 
+// Create a express server
 var app = express();
+
+//make the server use port spesified in _config/main.js
 app.set('port',config.port);
 
-// routes --> extract routes into different express.router() middleware thingies:
+// Set the server to listen to all the different routes declared in router.js
 app.use('/', router);
 
+// Start the server, and listen to the spesified port.
 app.listen(app.get('port'), function() {
   console.log("server is running on port " + app.get('port'));
 });
