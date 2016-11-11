3dd8177baac66eb95eb50e4c8738faa9bc643033
S.Skaugvoll <skaugvoll@gmail.com>
2016-11-12 00:27:25 +0100

    added body-parser, morgan and connecting to db

--- server/server.js
+++ server/server.js
@@ -2,15 +2,37 @@ var express = require('express'); // Import express server framework
 var config = require("./_config/main.js"); // Import configurations
 var path = require('path'); // Import path-module
 var router = require('./router.js'); // Import main router with all routes
+var mongoose = require('mongoose'); // Import the database handler
+var bodyParser = require('body-parser'); // parse urlencoded bodies to JSON and expose the object in req.body
+
+var logger = require('morgan') // server request logger
+
 
 // Create a express server
 var app = express();
+
+// makes the server print to terminal all requests.
+app.use(logger('dev'));
 
 //make the server use port spesified in _config/main.js
 app.set('port',config.port);
 
 // Set the server to listen to all the different routes declared in router.js
 app.use('/', router);
+
+// Make the server able to listen extract content from POST body, and parse JSON
+app.use(bodyParser.urlencoded({ extended: false })); // extended choose between parsing the URL-encoded data with the "querystring" library(false) or the "qs" library(true)
+app.use(bodyParser.json()); // returns middleware that only parsers json
+
+// configure the database MongoDB
+// try and connect to the MongodDB spesified in the config file
+mongoose.connect(config.database, function(err, db){
+  if(err){ // if a error occured when trying to connect
+    console.log("Something went wrong when trying to connect to the mongoDB server. Error: " + err);
+  }
+  // Connected!!!
+  console.log('Successfully connected to: ' + config.database);
+});
 
 // Start the server, and listen to the spesified port.
 app.listen(app.get('port'), function() {
