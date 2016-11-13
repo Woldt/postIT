700774fbe0c6fc929db3cb8cfca7ae25cc59071e
S.Skaugvoll <skaugvoll@gmail.com>
2016-11-13 20:17:12 +0100

    changed to use database

--- server/_config/main.js
+++ server/_config/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //'database': 'mongodb://localhost:27017/postIT',
-  'database': '',
+  'database': 'mongodb://localhost:27017/postIT',
+  //'database': '',
   'port': process.env.PORT ||8080,
 }
