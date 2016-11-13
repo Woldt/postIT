59a9e83c86d296261a2731609acc5fea9f2770f0
S.Skaugvoll <skaugvoll@gmail.com>
2016-11-13 20:16:57 +0100

    added new function to save post request / new postit in database

--- server/_controllers/postitController.js
+++ server/_controllers/postitController.js
@@ -36,4 +36,31 @@ exports.searchByTitel = function(req, res, next) {
   console.log("tjohei");
   res.status(200).send(reqParam);
 
-}
+};
+
+exports.newPostit = function(req, res, next) {
+  var newPostit = req.body; // extract from the request body the key-value pairs
+
+  console.log(newPostit); // RB4P
+
+  // Check that all needed information is present and valid:
+  if(newPostit.title == '' || newPostit.category == '' || newPostit.description == ''){
+    return res.status(422).send({ error: 'Did you spessify title, category and description?' });
+  }
+
+  // If validation is passed, create a new postit:
+  var postit = new PostIT({
+    titel: newPostit.title,
+    category: newPostit.category.split(","), //create a list of the category keywords, split category on ','
+    description: newPostit.description
+  })
+
+  // Try and save to database
+  postit.save(function(err, newPostit){
+    if(err) {
+      return res.status(400).send({ message: 'something went wrong. Could not add new postit: ' + error})
+    }
+    // If no error, and saving a new postit was success. send this response
+    return res.status(200).send(newPostit + "\nSuccessfully saved");
+  });
+};
