465b8292186e5eb0c8be4f0240c785320f5fbab1
Christina Hellenes Andresen <christina.h.andresen@gmail.com>
2016-11-14 13:16:47 +0100

    created a form for adding a new movie

--- client/src/components/DatabasePosts/Posts.js
+++ client/src/components/DatabasePosts/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Posts.css';
 
 export default class Posts extends Component {
 
@@ -6,7 +7,10 @@ export default class Posts extends Component {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.state = {
-      resultSearch: ""
+      resultSearch: "",
+      inputTitle: "",
+      inputCategory: "",
+      inputDescription: "",
     };
   }
 
@@ -16,21 +20,47 @@ export default class Posts extends Component {
     });
   }
 
+  handleTitleChange(event){
+    this.setState({
+      inputTitle: event.target.value
+    })
+  }
+
+  handleCategoryChange(event){
+    this.setState({
+      inputCategory: event.target.value
+    })
+  }
+
+  handleDescriptionChange(event){
+    this.setState({
+      inputDescription: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>PostIT</h1>
-        <div>
+        <div className="InputSearch">
           <input type="text" value={this.state.resultSearch} onChange={this.handleChange} placeholder="søk i databasen" />
         </div>
+        <div className="InputAdd">
+          <form>
+            <input type="text" value={this.state.inputTitle} onChange={this.handleTitleChange} placeholder="Tittel" />
+            <input type="text" value={this.state.inputCategory} onChange={this.handleCategoryChange} placeholder="Kategori" />
+            <input type="text" value={this.state.inputDescription} onChange={this.handleDescriptionChange} placeholder="Beskrivelse" />
+            <button>Legg til</button>
+          </form>
+        </div>
         <table>
-          <tbody>
+          <tbody className="Table">
             <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Category</th>
-              <th>Description</th>
-              <th>Delete</th>
+              <th className="TableHeader">ID</th>
+              <th className="TableHeader">Title</th>
+              <th className="TableHeader">Category</th>
+              <th className="TableHeader">Description</th>
+              <th className="TableHeader">Delete</th>
             </tr>
             <tr>
 
