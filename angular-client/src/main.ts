3048d1216527e3a4ebb26f79f04c86ae02d2746e
Sigve Skaugvoll <skaugvoll@gmail.com>
2016-11-27 21:01:21 +0100

    added comment, needs to be doubled checked

--- angular-client/src/main.ts
+++ angular-client/src/main.ts
@@ -1,3 +1,10 @@
+/*
+This is the file, that makes our application run in the browser!
+it gets the "browser" module, then bootstrap the AppModule, which
+builds together the heart of our application (app.component)
+and serves it back to the "main.ts", which then returns the application to the
+index.html which in turn displays the application in the browser.
+*/
 import './polyfills.ts';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
