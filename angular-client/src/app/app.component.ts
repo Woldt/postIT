6650394e8b692d420ec32f2c9c93e0fb015f7c49
Sigve Skaugvoll <skaugvoll@gmail.com>
2016-11-27 20:59:13 +0100

    added comment, needs to be doubled checked

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -1,3 +1,8 @@
+/*
+This component is the "heart" of our application.
+It will be used as the "root"-component, that renders the different views
+in its .html template, all views will be shown in the <router-outler>.
+*/
 import { Component } from '@angular/core';
 
 @Component({
