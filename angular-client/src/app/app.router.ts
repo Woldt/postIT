085697071eadb6c0da3088ca38266d63d42be8d3
Sigve Skaugvoll <skaugvoll@gmail.com>
2016-11-27 21:01:01 +0100

    added comment, needs to be doubled checked

--- angular-client/src/app/app.router.ts
+++ angular-client/src/app/app.router.ts
@@ -1,3 +1,7 @@
+/*
+This is a module, that handles all the navigation
+between sub-pages / views of the application.
+*/
 import { RouterModule, Routes } from '@angular/router';
 
 import { Github } from './github/github.component';
@@ -6,6 +10,7 @@ import { Tutorial } from './tutorial/tutorial.component';
 
 
 const appRoutes: Routes = [
+    // Check to see if navigation / [routerLink]="['...']" matches any of these
     { path: '', component: Postit },
     { path: 'github', component: Github },
     { path: 'tutorial', component: Tutorial },
