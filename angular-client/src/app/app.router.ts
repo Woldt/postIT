38f57acf9322b0b27295e72f3b720012ab33b0d9
Sigve Skaugvoll <skaugvoll@gmail.com>
2016-11-27 17:37:56 +0100

    added tutorial as route

--- angular-client/src/app/app.router.ts
+++ angular-client/src/app/app.router.ts
@@ -2,11 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { Github } from './github/github.component';
 import { Postit } from './postIT/postit.component';
+import { Tutorial } from './tutorial/tutorial.component';
 
 
 const appRoutes: Routes = [
     { path: '', component: Postit },
     { path: 'github', component: Github },
+    { path: 'tutorial', component: Tutorial },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
