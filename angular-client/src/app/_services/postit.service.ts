69aa0bd42f76742b5721bd237590e36ba79cd63e
Sigve Skaugvoll <skaugvoll@gmail.com>
2016-11-29 19:39:04 +0100

    injected HTTP service

--- angular-client/src/app/_services/postit.service.ts
+++ angular-client/src/app/_services/postit.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@angular/core';
 
+import { Http, Response } from '@angular/http'; // this is needed to send, and listen to a HTTP request
+import { Observable } from 'rxjs/Observable'; // this is needed to make the service, return a Observable
+
 @Injectable()
 export class PostitService {
+  private apiUrl: String = "http://it2810-02.idi.ntnu.no:8080/api"; // remember to make this use the environment variable
+
+
+  // need a constructor to inject the HTTP-service
+  constructor(private http: Http) {} //The private lets us use the http globaly
+
+
+  // Create a method to fetch all from database!
+
+
+
 
 }
